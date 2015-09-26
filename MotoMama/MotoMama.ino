d8f88e19d83fe93f659cb34740ab44262e3e60fa
Bruno G. Eilliar <lord.eilliar@hotmail.com>
2015-09-26 12:33:36 -0300

    Control Motor movement using Serial communication.

--- MotoMama/MotoMama.ino
+++ MotoMama/MotoMama.ino
@@ -7,12 +7,12 @@ Date: September 26, 2015.
 Notes:
 - http://energia.nu/Tutorial_PWM.html
 - Motors: http://www.botnroll.com/index.php?id_product=290&controller=product&id_lang=5
-- For a 20V Input voltage -> PWM 45% duty cicle
+- For a 20V Input voltage -> PWM 45% duty cicle to emulate 6V 
 */
-
+boolean debug = true;
 const int EnB = P1_7;
-const int In4 = P2_1;
-const int In3 = P2_2;
+const int In4 = P2_1;  // Use pin with analogWrite support
+const int In3 = P2_2;  // Use pin with analogWrite support
 
 void forward(int En, int InX, int InY)
 {
@@ -29,6 +29,19 @@ void backward(int En, int InX, int InY)
   analogWrite(InY, 76);  // PWM 45% Duty Cicle
   digitalWrite(En, HIGH);
 }
+void fastStop(int En, int InX, int InY)
+{
+  digitalWrite(En, HIGH);
+  digitalWrite(InX, HIGH);
+  digitalWrite(InY, HIGH);
+}
+
+void freeStop(int En, int InX, int InY)
+{
+  digitalWrite(En, LOW);
+  digitalWrite(InX, LOW);
+  digitalWrite(InY, LOW);
+}
 
 void setup()
 {
@@ -36,18 +49,35 @@ void setup()
   pinMode(EnB, OUTPUT);
   pinMode(In3, OUTPUT);
   pinMode(In4, OUTPUT);
-  digitalWrite(EnB, LOW);
+  freeStop(EnB, In3, In4);
+  Serial.begin(9600);
 }
 
 void loop()
 {
+  char skipChar = '\n';
   // put your main code here, to run repeatedly:
-  forward(EnB, In3, In4);
-  delay(5000);
-  digitalWrite(EnB, LOW);
-  delay(100);
-  backward(EnB, In3, In4);
-  delay(5000);
-  digitalWrite(EnB, LOW);
-  delay(100);
+  while(Serial.available() > 0)
+  {
+    // look for the information: Forward = 1; Backward = 2
+    int movement = Serial.parseInt();
+    if (debug)
+    {Serial.print("Data Received!");}
+    if (Serial.read() == '\n')
+    {
+      if (movement == 1)
+      {
+        forward(EnB, In3, In4);
+      }
+      else if (movement == 2)
+      {
+        backward(EnB, In3, In4);
+      }
+      else
+      {
+        freeStop(EnB, In3, In4);
+        Serial.print("Error! Movement must be: (1) Foward or (2) Backward.");
+      }
+    }  
+  }
 }
